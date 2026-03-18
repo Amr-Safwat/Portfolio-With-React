ce7ca3f65f5c5d58bb0f4c6a03c46f216a1d1513
Amr-Safwat <amrsafwat02@gmail.com>
2026-03-18 02:58:54 +0200

    add wow library

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import './about.css'
-import SectionTitle from '../sec-title/SectionTitle'
-import Image from '../../assets/image.png'
+import React from 'react';
+import './about.css';
+import SectionTitle from '../sec-title/SectionTitle';
+import Image from '../../assets/image.png';
 import Button from '../button/Button';
 import cvUrl from '../../assets/Amr_Safwat_CV_With_Project_Links.pdf';
 
@@ -10,55 +10,49 @@ function About() {
     <div className="about-sec section-p">
       <SectionTitle sectionName="ABOUT ME" />
       <div className="container">
-        <div className="left-section">
+        <div className="left-section wow slideInRight" data-wow-delay=".1s">
           <img src={Image} alt="" />
         </div>
-        <div className="right-section">
-          <h1 className="name">Hi There! I'm Amr Safwat</h1>
-          <h3 className="jop-title">Front End Developer</h3>
-          <p>
+        <div className="right-section wow slideInRight" data-wow-delay=".2s">
+          <h1 className="name wow slideInRight" data-wow-delay=".3s">
+            Hi There! I'm Amr Safwat
+          </h1>
+          <h3 className="jop-title wow slideInRight" data-wow-delay=".4s">
+            Front End Developer
+          </h3>
+          <p className="wow slideInRight" data-wow-delay=".5s">
             I am a Visual Designer with a strong focus on digital branding.
             Visul design seeks to attract, inspire, create desires and otivate
             people to respond to messages, with a view to making a favorable
             impact.
           </p>
           <ul className="Birthday-info">
-            <li>
+            <li className="wow slideInRight" data-wow-delay=".6s">
               <span>Birthday</span> : <span>July 05, 2001</span>
             </li>
-            <li>
+            <li className="wow slideInRight" data-wow-delay=".7s">
               <span>Phone</span> : <span>+201033792397</span>
             </li>
-            <li>
+            <li className="wow slideInRight" data-wow-delay=".8s">
               <span>Email</span> : <span>amrsafwat02@gmail.com</span>
             </li>
-            <li>
+            <li className="wow slideInRight" data-wow-delay=".9s">
               <span>From</span> : <span>Cairo, Egypt</span>
             </li>
-            <li>
+            <li className="wow slideInRight" data-wow-delay="1s">
               <span>Language</span> : <span>English, Arabic</span>
             </li>
-            <li>
+            <li className="wow slideInRight" data-wow-delay="1.1s">
               <span>Freelance</span> : <span>Available</span>
             </li>
           </ul>
-          <Button text="Download CV" cv={true} url={cvUrl} />
+          <div className="wow slideInRight" data-wow-delay="1.2s">
+            <Button text="Download CV" cv={true} url={cvUrl} />
+          </div>
         </div>
-        <div className="wow" style={{color: 'red'}}>Content to Reveal Here</div>
-        <section
-          className="wow slideInLeft"
-          data-wow-duration="2s"
-          data-wow-delay="5s"
-        ></section>
-
-        <section
-          className="wow slideInRight"
-          data-wow-offset="10"
-          data-wow-iteration="10"
-        ></section>
       </div>
     </div>
   );
 }
 
-export default
+export default About;
--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -5,10 +5,17 @@ function Hero() {
     <div className="hero section-p">
       <div className="container">
         <div className="sec-left">
-          <span className="hello">Hello, I'm</span>
-          <div className="name">Amr Safwat</div>
-          <div className="jop-title">Front End Developer</div>
-          <button className="hire-me">Hire Me</button>
+          <span className="hello wow bounceInUp">Hello, I'm</span>
+          <div className="name wow bounceInUp" data-wow-delay=".2s">
+            Amr Safwat
+          </div>
+
+          <div className="jop-title wow bounceInUp" data-wow-delay=".4s">
+            Front End Developer
+          </div>
+          <button className="hire-me wow bounceInUp" data-wow-delay=".6s">
+            Hire Me
+          </button>
         </div>
         <div className="sec-right">
           <img src="" alt="" />
