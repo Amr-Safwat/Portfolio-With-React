529d00d893415c03e47406ac23f5b28ea8d36d65
Amr-Safwat <amrsafwat02@gmail.com>
2026-03-19 10:29:59 +0200

    add some edits

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import {base} from 'motion/react-client';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-With-React/',
+  base: '/Portfolio-With-React',
 });
