9b3e2793a34f883fdfeb6ab07d4fe99993dd15da
Amr-Safwat <amrsafwat02@gmail.com>
2026-04-12 00:41:21 +0200

    added Image to hero section

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './hero.css';
 import Button from '../button/Button';
+import Image from '../../assets/image.png';
+import {FaGithub} from 'react-icons/fa';
+import {IoLogoLinkedin} from 'react-icons/io';
+import {FaFacebook} from 'react-icons/fa';
+import {RiWhatsappFill} from 'react-icons/ri';
+
 function Hero() {
   return (
     <div id="home" className="hero section-p">
@@ -18,8 +24,22 @@ function Hero() {
             <Button text={'Hire Me'} />
           </div>
         </div>
-        <div className="sec-right">
-          <img src="" alt="" />
+        <div className="sec-right  wow fadeInRight" data-wow-delay=".8s">
+          <img src={Image} alt="Amr Safwat" />
+          <div className="social-icons wow fadeInLeft" data-wow-delay="1s">
+            <a href="">
+              <FaGithub />
+            </a>
+            <a href="">
+              <IoLogoLinkedin />
+            </a>
+            <a href="">
+              <FaFacebook />
+            </a>
+            <a href="">
+              <RiWhatsappFill />
+            </a>
+          </div>
         </div>
       </div>
     </div>
